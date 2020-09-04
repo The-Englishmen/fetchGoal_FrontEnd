6012f62227d78d4bb40cddcc8603ac7c98734a0b
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-09-04 12:12:31 -0700

    react app ready

---
+++ fetchgoal/src/Components/Leagues.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Leagues extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default Leagues;
---
+++ fetchgoal/src/Components/Starplayer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Starplayer extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default Starplayer;
